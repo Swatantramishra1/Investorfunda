2b5fe2b4bb1cbee611c96465198552cc20afc7bd
Swatantra Kumar Mishra <swatantramishra1@gmail.com>
2017-04-11 00:00:44 +0530

    final view of page

--- Script/Module/app.js
+++ Script/Module/app.js
@@ -296,3 +296,52 @@ app.service('fileUpload', ['$http', function ($http) {
         });
     }
 }]);
+app.factory('GetCommonData', ['$rootScope', function ($rootScope) {
+
+    var _factoryData = {};
+    getChildCommonData = {
+        "Name1": "COURSE FEE",
+        "Name2": "LIVING EXPENSES",
+        "Name3": "LUMPSUM AMOUNT",
+        "isShowDataChild": true
+    },
+     getRetirementCommonData = {
+         "Name1": "Current Age",
+         "Name2": "Retirement Age",
+         "Name3": "Currently Expenditure AMOUNT",
+         "isShowDataRetirement": true
+     },
+     getBuyHouseCommonData = {
+         "Name1": "Current Cost Of Home",
+         "Name2": "Your Loan Amount",
+         "Name3": "Your Monthly EMI",
+         "isShowDataBuyHouse": true
+     }
+    getBuyCarCommonData = {
+        "Name1": "Year To Achieve Goal",
+        "Name2": "Month To Achieve Goal",
+        "Name3": "Goal Amount",
+        "isShowDataBuyCar": true
+    }
+    getChildMarrigeCommonData = {
+        "Name1": "Budget For Wedding",
+        "Name2": "Year For Wedding",
+        "Name3": "Goal Year",
+        "isShowDataChildMarrige": true
+    }
+    getChildTourCommonData = {
+        "Name1": "Budget For World Tour",
+        "Name2": "Year For Tour",
+        "Name3": "Monthly Amount",
+        "isShowDataTour": true
+    }
+    return _factoryData = {
+        getChildCommonData: getChildCommonData,
+        getRetirementCommonData: getRetirementCommonData,
+        getBuyHouseCommonData: getBuyHouseCommonData,
+        getBuyCarCommonData: getBuyCarCommonData,
+        getChildMarrigeCommonData: getChildMarrigeCommonData,
+        getChildTourCommonData: getChildTourCommonData
+    }
+
+}]);
